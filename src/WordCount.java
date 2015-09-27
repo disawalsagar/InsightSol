80c532a772647106340eb7e328faab8fd5425cad
disawalsagar <disawalsagar@yahoo.com>
2015-09-27 16:54:37 -0500

    second

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,19 +1,22 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
+import com.sun.xml.internal.bind.v2.runtime.reflect.Lister.Pack;
+
 public class WordCount {
 
 	public static void wordCount() {
-		
 
 		FileReader fileReader = null;
 		BufferedReader fileBufferedReader;
@@ -21,9 +24,10 @@ public class WordCount {
 		BufferedWriter fileBufferedWriter;
 		String line;
 		try {
-			fileReader = new FileReader("C:\\Users\\Sagar.Disawal\\Desktop\\test.txt");
+			Path path = ;
+			fileReader = new FileReader("..\\tweet_input\\tweets.txt");
 			fileBufferedReader = new BufferedReader(fileReader);
-			fileWriter = new FileWriter("C:\\Users\\Sagar.Disawal\\Desktop\\testoutput.txt");
+			fileWriter = new FileWriter("\\InsightSol\\tweet_output\\ft1.txt");
 			fileBufferedWriter = new BufferedWriter(fileWriter);
 			Map<String, Integer> wordMap = new HashMap<String, Integer>();
 			line = fileBufferedReader.readLine();
@@ -48,11 +52,6 @@ public class WordCount {
 				fileBufferedWriter.write(wordCount.getKey() + " " + wordCount.getValue());
 				fileBufferedWriter.newLine();
 			}
-
-			// System.out.println("Done");
-			// long startTime = System.currentTimeMillis();
-			// long estimatedTime = System.currentTimeMillis() - startTime;
-			// System.out.println(estimatedTime);
 
 		} catch (FileNotFoundException e) {
 			System.err.println(e);
