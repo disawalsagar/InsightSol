acae2e7c1e4f71b57bc3b109b9792cacbdc055be
disawalsagar <disawalsagar@yahoo.com>
2015-09-29 22:08:29 +0530

    save

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-
+path=$(pwd)
+tweet_input='/tweet_input/tweets.txt';
+tweet_output='/tweet_output';
+complete_tweet_input_path=$path$tweet_input
+complete_src_path=$path
+complete_tweet_output_path=$path$tweet_output
+cd src; javac WordCount.java;java WordCount $complete_tweet_input_path $complete_tweet_output_path; cd ..;
+cd src; javac FindMedian.java;java FindMedian $complete_tweet_input_path $complete_tweet_output_path; cd ..;
