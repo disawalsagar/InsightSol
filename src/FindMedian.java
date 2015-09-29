29ee376f9a651e0f8a3672b52dcf20fca71c1f3d
disawalsagar <disawalsagar@yahoo.com>
2015-09-29 01:05:28 -0500

    Update FindMedian.java

--- src/FindMedian.java
+++ src/FindMedian.java
@@ -1,13 +1,12 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
@@ -20,37 +19,50 @@ public class FindMedian {
 	BufferedWriter fileBufferedWriter;
 	String line = null;
 	Queue<Integer> minWordsHeap = new PriorityQueue<Integer>();
-	Queue<Integer> maxWordsHeap = new PriorityQueue<Integer>(10, new MyComparator());
-	int numOfWords;
+	Queue<Integer> maxWordsHeap = new PriorityQueue<Integer>(50,
+			new MyComparator());
+	int numOfWords = 0;
 
-	public void findMedianByTweet() throws IOException {
+	public void findMedianByTweet(String input, String output)
+			throws IOException {
 
 		try {
-			fileReader = new FileReader("C:\\Users\\Sagar.Disawal\\Desktop\\test.txt");
+			fileReader = new FileReader(input);
 			fileBufferedReader = new BufferedReader(fileReader);
-			fileWriter = new FileWriter("C:\\Users\\Sagar.Disawal\\Desktop\\testoutput.txt");
+			fileWriter = new FileWriter(output + File.separator + "ft2.txt");
 			fileBufferedWriter = new BufferedWriter(fileWriter);
+
+			Set<String> wordSet = new HashSet<String>();
+			line = fileBufferedReader.readLine();
+
+			while (line != null) {
+				String[] words = line.split("\\s");
+				wordSet.clear();
+				for (String wordKey : words) {
+					wordSet.add(wordKey);
+				}
+				fileBufferedWriter.write(String.valueOf(populateQueue(Integer
+						.valueOf(wordSet.size()))));
+				fileBufferedWriter.newLine();
+				line = fileBufferedReader.readLine();
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		Set<String> wordSet = new HashSet<String>();
-		line = fileBufferedReader.readLine();
+		} finally {
 
-		while (line != null) {
-			String[] words = line.split("\\s");
-			wordSet.clear();
-			for (String wordKey : words) {
-				wordSet.add(wordKey);
+			try {
+				fileBufferedReader.close();
+				fileBufferedWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			wordSet.size();
-			System.out.println(getMedia(Integer.valueOf(wordSet.size())));
-			line = fileBufferedReader.readLine();
+
 		}
 	}
 
-	public double getMedia(Integer num) {
+	public double populateQueue(Integer num) {
 		maxWordsHeap.add(num);
 
 		if (numOfWords % 2 == 0) {
@@ -78,14 +90,14 @@ public class FindMedian {
 	}
 
 	private class MyComparator implements Comparator<Integer> {
-		public int compare(Integer o1, Integer o2) {
-			return o2 - o1;
+		public int compare(Integer i1, Integer i2) {
+			return i2 - i1;
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		FindMedian FindMedian = new FindMedian();
-		FindMedian.findMedianByTweet();
+		FindMedian.findMedianByTweet(args[0], args[1]);
 
 	}
 }
